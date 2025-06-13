f9285ccaaa0d6a0ec8f1dc3a61210233a500e3e8
techmentors2024 <techmentors2024@gmail.com>
2025-06-13 18:48:36 +0700

    config role access with keycloak

--- src/main/java/dev/chhaya/customer/security/SecurityConfig.java
+++ src/main/java/dev/chhaya/customer/security/SecurityConfig.java
@@ -2,11 +2,21 @@ package dev.chhaya.customer.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +28,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(endpoints ->
                 endpoints
                         .requestMatchers("/api/customers/public").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/customers").hasRole("ADMIN")
                         .anyRequest().authenticated()
         );
 
@@ -33,6 +44,27 @@ public class SecurityConfig {
         );
 
         return http.build();
+    }
+
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
+
+        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+
+            Collection<String> roles = realmAccess.get("roles");
+
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+        };
+
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
     }
 
 }
