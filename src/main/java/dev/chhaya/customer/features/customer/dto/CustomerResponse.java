5111ee97cc41662aae94671b0ff75c5774305267
techmentors2024 <techmentors2024@gmail.com>
2025-09-03 19:32:40 +0700

    update customr resposne

--- src/main/java/dev/chhaya/customer/features/customer/dto/CustomerResponse.java
+++ src/main/java/dev/chhaya/customer/features/customer/dto/CustomerResponse.java
@@ -2,12 +2,16 @@ package dev.chhaya.customer.features.customer.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 @Builder
 public record CustomerResponse(
         String customerNo,
         String firstName,
         String lastName,
         String email,
+        LocalDate dateOfBirth,
+        String phoneNumber,
         String segment
 ) {
 }
