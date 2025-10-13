b94d9a1fc4bcc9c5011bd598a769c8b78f5ce443
techmentors2024 <techmentors2024@gmail.com>
2025-10-13 18:22:39 +0700

    consume customer created event topic

---
+++ src/main/java/dev/chhaya/customer/features/customer/CustomerEventConsumer.java
@@ -0,0 +1,26 @@
+package dev.chhaya.customer.features.customer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.chhaya.customer.features.customer.dto.CustomerSyncDto;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomerEventConsumer {
+
+    private final ObjectMapper objectMapper;
+    private final CustomerService customerService;
+
+    @KafkaListener(topics = "customerCreatedEventTopic",
+        groupId = "${spring.application.name}")
+    public void consumeCustomerCreatedEvent(ConsumerRecord<String, Object> record) throws JsonProcessingException {
+        CustomerSyncDto customerSyncDto = objectMapper
+                .readValue(record.value().toString(), CustomerSyncDto.class);
+        customerService.syncCustomer(customerSyncDto);
+    }
+
+}
